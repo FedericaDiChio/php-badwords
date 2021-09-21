73d9d027f67bd144cb37bed727ba9da023a5e21f
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-21 15:44:42 +0200

    variabile e stampa in pagina

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
     <title>Bad Words</title>
 </head>
 <body>
-    
+    <!-- Creare una variabile con un paragrafo di testo a vostra scelta. -->
+    <p>
+        <?php 
+            $text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sit sed do eiusmod tempor incididunt sit ut labore et dolore sit magna aliqua sit.";
+            $message = "Le parole contenute nel testo sono: ";
+            echo $text;
+        ?>
+    </p>
+    <!-- Stampare a schermo il paragrafo e la sua lunghezza.  -->
+    <p>
+        <?php 
+        echo $message;
+        echo strlen($text);  
+        ?>
+    </p>
 </body>
 </html>
